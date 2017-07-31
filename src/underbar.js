19777c8131f5740947da51651c6eb64b9d92bac6
Tiffany Pham <tiffanynpham@gmail.com>
2017-07-30 18:54:39 -0700

    Completed functions up to _each.

--- src/underbar.js
+++ src/underbar.js
@@ -10,6 +10,8 @@
     return val;
   };
 
+
+
   /**
    * COLLECTIONS
    * ===========
@@ -47,6 +49,16 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+
+    if (Array.isArray(collection) === true) {
+      for (var i = 0; i < collection.length; i++) {
+        return iterator(collection[i], i, collection);
+      }
+    } else if (typeof collection === "object") {
+      for (var key in collection) {
+        return iterator(collection[key], key, collection);
+      }
+    };
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
